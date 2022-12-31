dd020bdf6ff4147fc16c9b781e5cc26e1b7c9b31
Almahari-A <a7md13ebrahim@gmail.com>
2022-12-31 16:32:56 +0000

    User seeder

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $a = new User;
+        $a-> name = "Ali";
+        $a-> email = "Ali@email.com";
+        $a-> password = "Ali123";
+        $a-> save();
+    }
+}
