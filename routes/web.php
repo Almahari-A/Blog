bd251ef84e7933e28136158d9fc1958139ac22cc
Almahari-A <a7md13ebrahim@gmail.com>
2023-01-01 18:02:14 +0000

    user controller , route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +21,6 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('layouts.app');
 });
+
+Route::get('/users', [UserController::class, 'index']);
 
