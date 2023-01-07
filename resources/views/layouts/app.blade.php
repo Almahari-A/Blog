f06c75945be13e6b7b3421ed337bfb371d8981d3
Almahari-A <a7md13ebrahim@gmail.com>
2023-01-07 23:18:27 +0000

    fixing layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,15 +32,23 @@
             {{ $slot }}
                 <body>
                     
-                    <div>
-                        @yield('content')
-                    </div>
+                    <div class="py-12">
+                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                <div class="p-6 text-gray-900">
+                                    <div>
+                                        @yield('content')
+                                    </div>
 
-                    <div>
-                    @if (session('message'))
-                    <p><b>{{ session('message') }}</b></p>
-                    @endif
-                    </div> 
+                                    <div>
+                                    @if (session('message'))
+                                    <p><b>{{ session('message') }}</b></p>
+                                    @endif
+                                    </div> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                 </body>
 
